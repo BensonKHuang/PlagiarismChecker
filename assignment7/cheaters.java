7888f1d9ec40a13a30dcce0603c969f164109ca7
Benson <bensonkhuang@gmail.com>
2018-04-23 15:37:38 -0500

    first commit

---
+++ assignment7/cheaters.java
@@ -0,0 +1,14 @@
+package assignment7;
+
+public class cheaters {
+    public static void main(String [] args){
+
+        String fileName = args[0];
+        int phraseSize = Integer.parseInt(args[1]);
+
+        //do something
+
+    }
+    
+
+}
